58d7954f615ab79797e6702dad6b9acc69698f9a
Mitch Webster <mitch83083@gmail.com>
2019-09-19 18:26:52 -0400

    update state working

--- clicky/src/components/Wrapper/index.js
+++ clicky/src/components/Wrapper/index.js
@@ -11,20 +11,23 @@ class CardWrapper extends Component {
   }
 
   resetState = () => {
-    const arr = [...this.state.friends]
-    const resetClick = arr.map(friend => (friend.clicked = false))
-    console.log(resetClick)
+    const resetClick = this.state.friends.map(friend => {
+      const obj = { ...friend, clicked: false }
+      return obj
+    })
     return this.setState({
-      // friends: [...friends, ...resetClick],
+      friends: [...resetClick],
       score: 0
     })
   }
 
   score = arr => this.setState({ score: arr.length })
 
-  topScore = arr => {
-    if (arr.length > this.state.topScore) {
-      return this.setState({ topScore: arr.length })
+  topScore = () => {
+    console.log(this.state.score)
+    if (this.state.score > this.state.topScore) {
+      console.log(this.state.score)
+      return this.setState({ topScore: this.state.score })
     }
   }
 
@@ -33,11 +36,32 @@ class CardWrapper extends Component {
   cardAlreadySelected = () =>
     this.state.friends.filter(friend => friend.clicked)
 
+  findIndex = (id, arr) => arr.map(obj => obj.id).indexOf(id)
+
+  update = (i, val, list) => {
+    return [...list.slice(0, i), val, ...list.slice(i + 1)]
+  }
+
+  updateState = id => {
+    const cardClicked = this.cardSelected(id)[0]
+    const cardObj = { ...cardClicked, clicked: true }
+    const index = this.findIndex(id, this.state.friends)
+    const newFriends = this.update(index, cardObj, this.state.friends)
+
+    return this.setState({
+      friends: newFriends,
+      score: this.state.score + 1
+    })
+    // console.log(id)
+    // console.log(arr)
+  }
+
   gameLogic = id => {
     if (this.cardAlreadySelected().includes(...this.cardSelected(id))) {
       return this.resetState()
     }
-    return console.log('not yet selected')
+    this.updateState(id)
+    return this.topScore()
   }
 
   render() {
